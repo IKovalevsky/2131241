fd81ecbb3b682f4d24ab163494e9d24c305455ad
kiragermanica <kira_germanica@outlook.com>
2024-05-25 17:38:17 +0300

    bash script for simulation of device env

---
+++ devices/device_imitation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+devices="devices.txt"
+
+> "$devices"
+
+run_script() {
+  local script_name=$1
+  local start_port=$2
+  local end_port=$3
+  local device_name=$4
+
+  for ((port=start_port; port<=end_port; port++)); do
+    delay=$((RANDOM % 2001 + 500))
+    
+    python3 "$script_name" "$port" "$delay" &
+    
+    echo "$device_name:$port:$delay" >> "$devices"
+  done
+}
+
+run_script "windows.py" 10001 10003 "windows"
+
+run_script "switch.py" 10021 10023 "switch"
+
+echo "Информация о запусках записана в $devices."
+
