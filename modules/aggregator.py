ff2e4c3d782aadf1e94591b38d31b565f38c7826
IKovalevsky <ivan.kavalski927@gmail.com>
2024-05-26 17:12:09 +0300

    start aggregator

---
+++ modules/aggregator.py
@@ -0,0 +1,22 @@
+import multiprocessing as mp
+import sqlite3
+
+
+
+class aggregator:
+    def __init__(self, queqe) -> None:
+        self.queqe = queqe
+
+    def parsing():
+        #парсинг
+        pass
+
+    def filter():
+        #фильтрует как-то 
+        pass
+
+    def bd():
+        #запись в бд 
+        pass
+
+        
